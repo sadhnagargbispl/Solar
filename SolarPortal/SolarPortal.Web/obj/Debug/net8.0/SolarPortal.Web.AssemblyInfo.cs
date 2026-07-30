9be3833b2ddd3f587ec4e725f7d5e6572b9bb049
sadhnagargbispl <99010109+sadhnagargbispl@users.noreply.github.com>
2026-07-30 11:48:38 +0530

    1

--- SolarPortal/SolarPortal.Web/obj/Debug/net8.0/SolarPortal.Web.AssemblyInfo.cs
+++ SolarPortal/SolarPortal.Web/obj/Debug/net8.0/SolarPortal.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SolarPortal.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7132d78c8cc9d8301d8489449205238f97f8e68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a09388d1089b7ae5cb318f8715cf68aa1ba31011")]
 [assembly: System.Reflection.AssemblyProductAttribute("SolarPortal.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SolarPortal.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
